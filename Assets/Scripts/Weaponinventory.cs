ada713b6a6dc0abecd335e4cc2933a80f0b35b9b
Snap-Trap <111644953+Snap-Trap@users.noreply.github.com>
2024-03-21 14:28:13 +0100

    Weapon switching

--- Assets/Scripts/Weaponinventory.cs
+++ Assets/Scripts/Weaponinventory.cs
@@ -4,44 +4,30 @@ using UnityEngine;
 
 public class Weaponinventory : MonoBehaviour
 {
-    public enum WeaponType { Pistol, Shotgun, AssaultRifle };
-
-    public Weapon[] weapons; // Array of weapon prefabs
-
-    // Basic Weapon class (you can expand on this based on your needs)
-    public class Weapon
-    {
-        // Add weapon properties like damage, fire rate, etc. (optional)
-    }
-
-    private WeaponType currentWeapon;
-
-    void Start()
-    {
-        currentWeapon = WeaponType.Pistol; // Start with pistol
-    }
+    public Shooting shooting;
 
     void Update()
     {
-        // Check for key presses (1, 2, or 3)
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-            currentWeapon = WeaponType.Pistol;
+            shooting.isPistol = true;
+            shooting.isShotgun = false;
+            shooting.isAssaultRifle = false;
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            currentWeapon = WeaponType.Shotgun;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            currentWeapon = WeaponType.AssaultRifle;
-        }
-    }
 
-    // Function to equip the current weapon (can be used for visuals or firing)
-    public Weapon GetCurrentWeapon()
-    {
-        return weapons[(int)currentWeapon];
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            shooting.isPistol = false;
+            shooting.isShotgun = true;
+            shooting.isAssaultRifle = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            shooting.isPistol = false;
+            shooting.isShotgun = false;
+            shooting.isAssaultRifle = true;
+        }
     }
 }
 
